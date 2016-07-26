18812440c8d50838f4b8f9cd5e4b2bf54055e54f
Alexander Tomlinson <mexander@gmail.com>
2016-07-26 10:39:24 -0700

    init commit

---
+++ ripbot.py
@@ -0,0 +1,32 @@
+from groupy import Bot
+from flask import Flask, request
+
+import json
+
+app = Flask(__name__)
+log = app.logger
+
+
+class GroupMeBot(object):
+    """
+    Simple groupme bot
+    """
+    def __init__(self, post):
+        self.post = post
+
+    def callback(self):
+        """
+        Method to send responses on callbacks.
+        """
+        data = json.loads(request.data.decode('utf8'))
+        print data
+
+
+if __name__ == '__main__':
+    which_bot = 'ripbot'
+    bot = Bot.list().filter(name=which_bot)[0]
+
+    ripbot = GroupMeBot(bot.post)
+
+    app.route('/', methods=['POST'])(ripbot.callback)
+    app.run('0.0.0.0', port=5000)
