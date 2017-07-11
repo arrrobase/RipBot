d7f0042c3d53e04fb1b822784f23776adcec37a4
tomlinsa <mexander@gmail.com>
2017-07-11 16:52:04 -0700

    only reply plusminus for every 10 cores

--- ripbot.py
+++ ripbot.py
@@ -361,8 +361,12 @@ class GroupMeBot(object):
                 post_text += '.'
                 post_text = post_text.format(points_to, points)
 
-            if points_to.lower() == 'core' and points == 500:
-                post_text += ' Congrats! You guys did it!'
+            if points_to.lower() == 'core':
+                if not points % 10 == 0:
+                    return None
+
+                if points == 500:
+                    post_text += ' Congrats! You guys did it!'
             
             return post_text
 
